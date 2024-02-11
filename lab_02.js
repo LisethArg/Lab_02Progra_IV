7fc5ffe3dfae422c6d8a520b99548d10730dfb30
LisethArg <riic14argueta@gmail.com>
2024-02-11 10:35:22 -0600

    agregre imag.jpg

---
+++ lab_02.js
@@ -0,0 +1,35 @@
+document.getElementById('box1').addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'red';
+});
+
+document.getElementById('box2').addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'silver';
+});
+
+document.getElementById('box3').addEventListener('mouseover', function() {
+    this.style.backgroundColor = 'blue';
+});
+
+document.getElementById('box4').addEventListener('click', function() {
+    this.style.backgroundColor = 'yellow';
+});
+
+document.getElementById('box5').addEventListener('click', function() {
+    this.style.backgroundColor = 'orange';
+});
+
+document.getElementById('box6').addEventListener('click', function() {
+    this.style.backgroundColor = 'purple';
+});
+
+document.getElementById('box7').addEventListener('focus', function() {
+    this.style.backgroundColor = 'pink';
+});
+
+document.getElementById('box8').addEventListener('focus', function() {
+    this.style.backgroundColor = 'cyan';
+});
+
+document.getElementById('box9').addEventListener('focus', function() {
+    this.style.backgroundColor = 'magenta';
+});
